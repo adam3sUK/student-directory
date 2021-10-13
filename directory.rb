ed62b3ae9a0d5995b6f2017b147d224286d0b838
Adam Harbisher <aharbisher@gmail.com>
2021-10-13 18:56:06 +0100

    Refractored code with methods

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-
 #first we create an array of students
 students = [
   "Dr. Hannibal Lecter",
@@ -14,10 +13,21 @@ students = [
   "Norman Bates"
 ]
 #then print them
-puts "The students of Villains Academy"
-puts "-----------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
 end
-# finally, we pinrt the total numbers of students
-print "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
